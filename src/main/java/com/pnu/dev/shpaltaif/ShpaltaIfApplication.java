686a962283de636e727305179b430a9baf032805
Ihor Polataiko <igor.bogdanovich39@gmail.com>
2020-09-05 23:35:44 +0300

    Initialized project

---
+++ src/main/java/com/pnu/dev/shpaltaif/ShpaltaIfApplication.java
@@ -0,0 +1,13 @@
+package com.pnu.dev.shpaltaif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShpaltaIfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShpaltaIfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pnu/dev/shpaltaif/ShpaltaIfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnu.dev.shpaltaif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShpaltaIfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
