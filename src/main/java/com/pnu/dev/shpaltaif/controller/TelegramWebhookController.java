9441caab874adb49f9b769bd49d30a84704280cd
Ihor Polataiko <igor.bogdanovich39@gmail.com>
2020-11-03 21:06:48 +0200

    Updated local dev settings

--- src/main/java/com/pnu/dev/shpaltaif/controller/TelegramWebhookController.java
+++ src/main/java/com/pnu/dev/shpaltaif/controller/TelegramWebhookController.java
@@ -6,17 +6,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.generics.WebhookBot;
 
 @RestController
 @RequestMapping("/telegram-bot-webhook-${telegrambot.url.secret}")
 public class TelegramWebhookController {
 
-    private TelegramBot telegramBot;
+    private WebhookBot telegramBot;
 
     @Autowired
-    public TelegramWebhookController(TelegramBot telegramBot) {
+    public TelegramWebhookController(WebhookBot telegramBot) {
         this.telegramBot = telegramBot;
     }
 
