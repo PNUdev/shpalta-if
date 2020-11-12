37a934f2652a51a79921b5f5425e1dadcedb80f3
Volodymyr Vynnyk <vovavinnik2707@gmail.com>
2020-11-12 15:42:14 +0200

    New UserServiceIntegrationTest

---
+++ src/test/java/com/pnu/dev/shpaltaif/integration/service/telegram/NewPublicAccountIntegrationTest.java
@@ -0,0 +1,6 @@
+package com.pnu.dev.shpaltaif.integration.service.telegram;
+
+import com.pnu.dev.shpaltaif.integration.BaseIntegrationTest;
+
+public class NewPublicAccountIntegrationTest extends BaseIntegrationTest {
+}
